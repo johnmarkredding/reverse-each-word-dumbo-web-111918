1fbeee317d3dfd2ffb100f2301c6ac3ac71b1958
John Mark Redding <johnmarkredding@gmail.com>
2018-11-09 04:00:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   arr = string.split(" ")
-  new_arr = []
-  arr.each {|item|
+  new_arr = arr.collect {|item|
     new_arr << item.reverse
   }
   string = new_arr.join(" ")
